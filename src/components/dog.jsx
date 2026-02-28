81e6cd0a2a1a1487a152beeab7fbc145e290487d
VivenG-2007 <25r11a05cm@gcet.edu.in>
2026-02-28 19:45:44 +0530

    update

--- src/components/dog.jsx
+++ src/components/dog.jsx
@@ -1,40 +1,46 @@
-import React, { useEffect } from 'react';
-import * as Three from 'three'
-import { useThree } from '@react-three/fiber';
-import { OrbitControls, useGLTF, useTexture } from '@react-three/drei';
-
+import React from 'react'
+import * as THREE from 'three'
+import { useThree } from '@react-three/fiber'
+import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
 
 export default function Dog() {
-    const model=  useGLTF('/models/dog.drc.glb');
-    useThree(({ camera,scene,gl }) => {
-      camera.position.set(-0.5, 0, 2.5);
-      gl.toneMapping=Three.ReinhardToneMapping;
-      gl.outputColorSpace=Three.SRGBColorSpace
-    });
-    const texture = useTexture({ normalMap: '/models/dog_normals.jpg',sampleMatCap:"/matcap/mat-2.png" },(texture)=>{
-    });
-    const[
-      normalMap,
-      sampleMatCap,
-    ]  =(useTexture(["/models/dog_normals.jpg","/matcap/mat-2.png"])).map((texture)=>{
-      texture.outputColorSpace=Three.SRGBColorSpace
-      texture.flipY=false;
-      return texture;
-    })
+  const { camera, gl } = useThree()
 
-   model.scene.traverse((child) => {
-    if(child.name.includes("DOG")){
-    child.material = new Three.MeshMatcapMaterial({
-  normalMap: texture.normalMap,
-  matcap:texture.sampleMatCap
-})
-  }
-})
+  // Camera + renderer setup
+  camera.position.set(-0.5, 0, 2.5)
+  gl.toneMapping = THREE.ReinhardToneMapping
+  gl.outputColorSpace = THREE.SRGBColorSpace
+
+  const model = useGLTF('/models/dog.drc.glb')
+
+  const textures = useTexture({
+    normalMap: '/models/dog_normals.jpg',
+    matcap: '/matcap/mat-2.png',
+  })
+
+  textures.normalMap.flipY = false
+  textures.matcap.colorSpace = THREE.SRGBColorSpace
+
+  // Apply material once
+  model.scene.traverse((child) => {
+    if (child.isMesh && child.name.includes('DOG')) {
+      child.material = new THREE.MeshMatcapMaterial({
+        normalMap: textures.normalMap,
+        matcap: textures.matcap,
+      })
+    }
+  })
+
   return (
     <>
-        <primitive object={model.scene} position={[0.5,-2.8,0]} scale={[5,5,5]} rotation={[0,0.2*Math.PI,0]} />
-        <directionalLight position={[1,5,5]} color={0xFFFFFF} intensity={10}/>
-        <OrbitControls />
+      <primitive
+        object={model.scene}
+        position={[0.5, -2.8, 0]}
+        scale={[5, 5, 5]}
+        rotation={[0, 0.2 * Math.PI, 0]}
+      />
+      <directionalLight position={[1, 5, 5]} intensity={10} />
+      <OrbitControls />
     </>
   )
 }
